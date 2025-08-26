adc3c6b9aaaec8f06b14f591588dbdfdb0cd0571
Huguito <110265653+BondiolaAlHorno@users.noreply.github.com>
2025-08-26 01:31:57 -0300

    Update main.py

---
+++ model/main.py
@@ -0,0 +1,19 @@
+from typing import Union
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+@app.get("/persona/{personita}")
+def persona(nombre:str="yo", apellido:str="no", dni:str="existo"):
+    return [nombre,apellido,dni]
