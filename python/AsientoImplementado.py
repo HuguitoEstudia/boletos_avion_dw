3633c5adcfc974981e169963a3e6525afa2f96bc
Huguito <110265653+BondiolaAlHorno@users.noreply.github.com>
2025-08-27 12:44:39 -0300

    Update AsientoImplementado.py

--- python/AsientoImplementado.py
+++ python/AsientoImplementado.py
@@ -1,6 +1,8 @@
-class AsientoImplementado:
-    def __init__(self,vuelo:Vuelo,disponible:bool):
+class AsientoImplementado(Asiento):
+    def __init__(self,numero:int,avion:Avion,estado:str,vuelo:Vuelo,disponible:bool):
+        super.__init__(numero,avion,estado)
         self.vuelo=vuelo
         self.disponible=disponible
 
 from Vuelo import Vuelo
+from Asiento import Asiento
